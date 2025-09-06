b2ae5f2b58f9aacf81e4928dded248141b227e4c
izzy-Ti <israelashenafi29@gmail.com>
2025-09-06 16:39:31 -0700

    Withdraw_func

--- contracts/Charity.sol
+++ contracts/Charity.sol
@@ -21,18 +21,25 @@ contract charity {
 
     mapping(uint => Charity) public charities;
 
-    function createCharity(string memory _name, address payable _charityAddress) public {
+    function createCharity(string memory _name) public {
         Charity storage newCharity = charities[charityCount] ;
         newCharity.name = _name;
-        newCharity.charityAddress = _charityAddress;
+        newCharity.charityAddress =payable (msg.sender);
         charityCount++;
     }
 
     function donate(uint charityIndex) public payable{
         uint _amount = msg.value;
         require(msg.value > 0.1 ether, "Amount less than minimum");
-        uint totalDonations = charities[charityIndex].totalDonations;
-        charities[charityIndex].totalDonations = totalDonations + _amount;
+        
+        charities[charityIndex].totalDonations = address(this).balance;
         charities[charityIndex].donors[msg.sender] = _amount;
+    }
+
+    function withdraw(uint charityIndex) public payable{
+        require(msg.sender == charities[charityIndex].charityAddress, "You are not owner");
+        uint balance = address(this).balance;
+        payable (msg.sender).transfer(balance);
+        charities[charityIndex].totalDonations = address(this).balance;
     }
 }
