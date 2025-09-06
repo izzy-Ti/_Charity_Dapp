8333b0a6fa298cbfe7ce1db9bcaa0ff3b8ab7310
izzy-Ti <israelashenafi29@gmail.com>
2025-09-06 16:12:52 -0700

    Charity create func

--- contracts/Charity.sol
+++ contracts/Charity.sol
@@ -4,7 +4,27 @@ pragma solidity 0.8.30;
 
 
 contract charity {
+    address owner;
+
+    uint public charityCount = 0;
+
     constructor() {
-        
+        owner = msg.sender;
+    }
+    struct Charity {
+        string name;
+        address payable charityAddress;
+        uint256 totalDonations;
+        uint256 fundsWithdrawn;
+        mapping(address => uint256) donors;
+    }
+
+    mapping(uint => Charity) public charities;
+
+    function createCharity(string memory _name, address payable _charityAddress) public {
+        charityCount++;
+        Charity storage newCharity = charities[charityCount] ;
+        newCharity.name = _name;
+        newCharity.charityAddress = _charityAddress;
     }
 }
